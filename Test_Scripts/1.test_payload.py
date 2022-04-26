220d18797a6e3548df0bf3f8bd9b8885a0db1738
Yaswanth Bobbu <yaswanth.bobbu@skf.com>
2022-04-26 20:17:04 +0530

    final commit

--- Test_Scripts/1.test_payload.py
+++ Test_Scripts/1.test_payload.py
@@ -1,5 +1,16 @@
+
 import xml.etree.ElementTree as ET
+from datetime import datetime, timedelta, date
 
 tree = ET.parse('../test_payload1.xml')
 root = tree.getroot()
+
+for data in root.findall('REQUEST'):
+    if data.find('TP'):
+        DEPART = data.find('TP').find('DEPART')
+        DEPART.text = str(date.today() + timedelta(days=2))
+        RETURN = data.find('TP').find('RETURN')
+        RETURN.text = str(date.today() + timedelta(days=2))
+
+tree.write('../out_test_payload1.xml')
 
